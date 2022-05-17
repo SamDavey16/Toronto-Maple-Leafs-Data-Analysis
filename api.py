d311ee44921196bfd39a37801fc1c046c554463b
SamDavey16 <32702240+SamDavey16@users.noreply.github.com>
2022-05-17 15:28:48 +0100

    updated api

--- api.py
+++ api.py
@@ -2,9 +2,31 @@ import requests
 import pandas as pd
 import tkinter as tk
 from tkinter import *
-from pandastable import Table, TableModel
+from tkinter import ttk
+#from pandastable import Table, TableModel
 
 df = pd.DataFrame(columns = ['Name', 'Games Played', 'Time On Ice', 'Assists', 'Goals', 'Shots', 'Rank'])
+
+goalie_df = pd.DataFrame(columns = ['Name', 'Games Played', 'Power Play Saves', 'Shots Against', 'Goals Against', 'Rank'])
+
+root = tk.Tk()
+
+root.geometry("1000x1000")
+root.pack_propagate(False) 
+root.resizable(0, 0) 
+
+
+frame1 = tk.LabelFrame(root, text="Excel Data")
+frame1.place(height=800, width=1000)
+
+tv1 = ttk.Treeview(frame1)
+tv1.place(relheight=1, relwidth=1) 
+
+treescrolly = tk.Scrollbar(frame1, orient="vertical", command=tv1.yview) 
+treescrollx = tk.Scrollbar(frame1, orient="horizontal", command=tv1.xview) 
+tv1.configure(xscrollcommand=treescrollx.set, yscrollcommand=treescrolly.set) 
+treescrollx.pack(side="bottom", fill="x") 
+treescrolly.pack(side="right", fill="y") 
 
 def get_goalie_ids():
     url = "https://statsapi.web.nhl.com/api/v1/teams/10/roster"
@@ -18,7 +40,7 @@ def get_goalie_ids():
         name = person["fullName"]
         ids = person["id"]
         if position["abbreviation"] == "G":
-            id_list.append(ids)
+            goalie_list.append(ids)
     return goalie_list
 
 def get_player_ids():
@@ -36,7 +58,7 @@ def get_player_ids():
             id_list.append(ids)
     return id_list
 
-def get_player_data(get_player_ids, df):
+def get_player_data(get_player_ids, df, get_goalie_ids, goalie_df):
     for x in get_player_ids():
         link = "https://statsapi.web.nhl.com/api/v1/people/" + str(x) + "/stats?stats=statsSingleSeason&season=20212022"
         name_link = "https://statsapi.web.nhl.com/api/v1/people/" + str(x)
@@ -65,11 +87,53 @@ def get_player_data(get_player_ids, df):
                     rank = 0
                 new_row = {'Name':fullname, 'Games Played':games_played, 'Time On Ice':toi, 'Assists':assists, 'Goals':goals, 'Shots':shots, 'Rank':rank}
                 df = df.append(new_row, ignore_index=True)
-    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
-        df = df.sort_values(by=['Rank'], ascending=False)
-        print(df)
-        
-
-
+    for x in get_goalie_ids():
+        link = "https://statsapi.web.nhl.com/api/v1/people/" + str(x) + "/stats?stats=statsSingleSeason&season=20212022"
+        name_link = "https://statsapi.web.nhl.com/api/v1/people/" + str(x)
+        name_link = requests.get(name_link)
+        name_link = name_link.json()
+        people = name_link["people"]
+        for l in people:
+            fullname = l["fullName"]
+        link = requests.get(link)
+        link = link.json()
+        stats = link["stats"]
+        for i in stats:
+            splits = i["splits"]
+            for i in splits:
+                stats2 = i["stat"]
+                pp_saves = int(stats2["powerPlaySaves"])
+                games = int(stats2["games"])
+                shots_against = int(stats2["shotsAgainst"])
+                goals_against = int(stats2["goalsAgainst"])
+                shutouts = int(stats2["shutouts"])
+                pps_percentage = float(stats2["powerPlaySavePercentage"])
+                shs_percentage = float(stats2["shortHandedSavePercentage"])
+                ess_percentage = float(stats2["evenStrengthSavePercentage"])
+                try:
+                    ranks = games / pps_percentage + shs_percentage + ess_percentage + games
+                except:
+                    ranks = 0
+                new_row = {'Name':fullname, 'Games Played':games, 'Power Play Saves':pp_saves, 'Shots Against':shots_against, 'Goals Against':goals_against, 'Rank':ranks}
+                goalie_df = goalie_df.append(new_row, ignore_index=True)
                 
-get_player_data(get_player_ids, df)
+
+    clear_data()
+    tv1["column"] = list(df.columns)
+    tv1["show"] = "headings"
+    for column in tv1["columns"]:
+        tv1.heading(column, text=column) 
+
+    df_rows = df.to_numpy().tolist() 
+    for row in df_rows:
+        tv1.insert("", "end", values=row) 
+    root.mainloop()
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+        goalie_df = goalie_df.sort_values(by=['Rank'], ascending=False)
+        print(goalie_df)
+        
+def clear_data():
+    tv1.delete(*tv1.get_children())
+    return None
+
+get_player_data(get_player_ids, df, get_goalie_ids, goalie_df)
